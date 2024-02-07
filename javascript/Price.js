171e86f12893db481d02075ce85ec5fa2ee058f2
abdul-basith-3 <abdulbasitha3003@gmail.com>
2024-02-08 00:48:15 +0530

    first commit

---
+++ javascript/Price.js
@@ -0,0 +1,139 @@
+const Product_A = 20;
+const Product_B = 40;
+const Product_C = 50;
+let wrap_a=0,wrap_b=0,wrap_c=0;
+let wrap_amount = 0;
+let shipping_fee =0;
+let total_amount=0;
+let cart_total_amount=0;
+let discount_name="";
+
+var dict = { flat_10_discount : 0 ,
+    bulk_5_discount : 0,
+    bulk_10_discount :0,
+    tiered_50_discount : 0
+};
+// fuction to cart total
+function cart_total(){
+    total = Product_A * a + Product_B *b + Product_C*c;
+    return total
+}
+//flat_10_discount
+function flat_10_discount(){
+    dict["flat_10_discount"]=10;
+}
+//bulk_5_discount
+function bulk_5_discount(){
+    let bulk_5_discount_for_a=0;
+    let bulk_5_discount_for_b=0;
+    let bulk_5_discount_for_c=0;
+    if(a>10){
+        bulk_5_discount_for_a=0.05*a*Product_A;
+    }
+    if(b>10){
+        bulk_5_discount_for_b=0.05*b*Product_B;
+    }
+    if(c>10){
+        bulk_5_discount_for_c=0.05*c*Product_C;
+    }
+    dict["bulk_5_discount"] = bulk_5_discount_for_a+bulk_5_discount_for_b+bulk_5_discount_for_c;
+}
+//bulk_10_discount
+function bulk_10_discount(){
+    dict["bulk_10_discount"]= cart_total_amount*0.1;
+}
+//tiered_50_discount
+function tiered_50_discount(){
+    if(a>15){
+        dict["tiered_50_discount"]+=(a-15)*Product_A*0.5;
+    }
+    if(b>15){
+        dict["tiered_50_discount"]+=(b-15)*Product_B*0.5;
+    }
+    if(c>15){
+        dict["tiered_50_discount"]+=(c-15)*Product_C*0.5;
+    }
+}
+//function to calculate total amount for wrapping
+function cal_wrap_amount(){
+    if(wrap_a){
+        wrap_amount+=a;
+    }
+    if(wrap_b){
+        wrap_amount+=b;
+    }
+    if(wrap_c){
+        wrap_amount+=c;
+    }
+}
+//function to calculate total amount for shipping
+function cal_shipping_fee(){
+    shipping_fee= Math.ceil(total_quantity/10) * 5;
+}
+//function to find maximum discount 
+function find_max_discount(){
+    const array = Object.values(dict);
+    return Math.max(...array);
+}
+//function to find name of maximum discount
+function find_discount_name(max_discount){
+     let name=Object.keys(dict).find(key=>dict[key]==max_discount);
+     if(dict[name]!=0){
+        return name;
+     }
+     else{
+        return "nill";
+     }
+}
+
+let a=parseInt(prompt("Enter the unit of PRODUCT A")) || 0;
+if(a!=0){
+     wrap_a = confirm("Are you ok with wraping the product");
+}
+let b=parseInt(prompt("Enter the unit of PRODUCT B")) || 0;
+if(b!=0){
+     wrap_b = confirm("Are you ok with wraping the product");
+}
+let c=parseInt(prompt("Enter the unit of PRODUCT C")) || 0;
+if(c!=0){
+     wrap_c = confirm("Are you ok with wraping the product");
+}
+total_quantity = a+b+c;
+cart_total_amount = cart_total();
+if (cart_total_amount >200){
+    flat_10_discount();
+}
+if(a>10 || b >10 || c>10){
+    bulk_5_discount();
+}
+if(total_quantity>20)
+{
+    bulk_10_discount();
+}
+if(total_quantity>30 && (a>15 || b>15 || c>15)){
+    tiered_50_discount();
+}
+cal_wrap_amount();
+cal_shipping_fee();
+const max_discount = find_max_discount();
+discount_name = find_discount_name(max_discount);
+total_amount = cart_total_amount - max_discount + wrap_amount+shipping_fee;
+console.log("PRODUCT NAME   QUANTITY    TOTAL AMOUNT");
+if(a!=0){
+    console.log("Product_A      "+a+"           "+Product_A*a);
+}
+if(b!=0){
+    console.log("Product_B      "+b+"           "+Product_B*b);
+}
+if(c!=0){
+    console.log("Product_C      "+c+"           "+Product_C*c);
+}
+console.log("");
+console.log("Subtotal"+"                   "+cart_total_amount);
+console.log("");
+console.log("Discount name : "+discount_name);
+console.log("Discount amount : "+max_discount);
+console.log("Shipping fee : "+shipping_fee);
+console.log("Gift wrap fee : "+wrap_amount);
+console.log("");
+console.log("Total : "+total_amount);
---
+++ python/main.py
@@ -0,0 +1,139 @@
+import math
+
+
+def cart_total():
+    return product['Product A']*a+product['Product B']*b+product['Product C']*c
+
+
+def flat_10_discount():
+    discount_dict['flat_10_discount'] = 10
+
+
+def bulk_5_discount():
+    bulk_5_discount_for_a = 0
+    bulk_5_discount_for_b = 0
+    bulk_5_discount_for_c = 0
+    if a > 10:
+        bulk_5_discount_for_a = 0.05 * a * product['Product A']
+    if b > 10:
+        bulk_5_discount_for_b = 0.05 * b * product['Product B']
+    if c > 10:
+        bulk_5_discount_for_c = 0.05 * c * product['Product C']
+    discount_dict['bulk_5_discount'] = bulk_5_discount_for_a + bulk_5_discount_for_b + bulk_5_discount_for_c
+
+
+def bulk_10_discount():
+    discount_dict['bulk_10_discount'] = cart_total_amount * 0.1
+
+
+def tiered_50_discount():
+    if a > 15:
+        discount_dict['tiered_50_discount'] += (a-15) * product['Product A'] * 0.5
+    if b > 15:
+        discount_dict['tiered_50_discount'] += (b-15) * product['Product B'] * 0.5
+    if c > 15:
+        discount_dict['tiered_50_discount'] += (c-15) * product['Product C'] * 0.5
+
+
+def find_max_discount():
+    list = discount_dict.values()
+    return max(list)
+
+
+def find_discount_name(max_discount):
+    key_list = list(discount_dict.keys())
+    value_list = list(discount_dict.values())
+    position = value_list.index(max_discount)
+    return key_list[position]
+
+
+def cal_wrap_amount():
+    wrap_amount = 0
+    if wrap_a:
+        wrap_amount += a
+    if wrap_b:
+        wrap_amount += b
+    if wrap_c:
+        wrap_amount += c
+    return wrap_amount
+
+
+def cal_shipping_fee():
+    return math.ceil(total_quantity/10)*5
+
+
+if __name__ == '__main__':
+    product = {
+        'Product A': 20,
+        'Product B': 40,
+        'Product C': 50
+    }
+    discount_dict = {
+        'flat_10_discount': 0,
+        'bulk_5_discount': 0,
+        'bulk_10_discount': 0,
+        'tiered_50_discount': 0
+    }
+    wrap_a, wrap_b, wrap_c = 0, 0, 0
+    a = int(input("Enter the unit of PRODUCT A :"))
+    if a:
+         wrap_a = input("Do you want to wrap the product (y/n) :")
+         if wrap_a=='y' or wrap_a=='Y':
+             wrap_a=1
+         elif wrap_a=='n' or wrap_a=='N':
+             wrap_a=0
+         else :
+             print("wrong choice")
+             exit()
+    b = int(input("Enter the unit of PRODUCT B :"))
+    if b:
+        wrap_b = input("Do you want to wrap the product (y/n) :")
+        if wrap_b == 'y' or wrap_b == 'Y':
+            wrap_b = 1
+        elif wrap_b == 'n' or wrap_b == 'N':
+            wrap_b = 0
+        else:
+            print("wrong choice")
+            exit()
+    c = int(input("Enter the unit of PRODUCT C :"))
+    if c:
+        wrap_c = input("Do you want to wrap the product (y/n) :")
+        if wrap_c == 'y' or wrap_c == 'Y':
+            wrap_c = 1
+        elif wrap_c == 'n' or wrap_c == 'N':
+            wrap_c = 0
+        else:
+            print("wrong choice")
+            exit()
+    total_quantity = a+b+c
+    cart_total_amount = cart_total()
+    if cart_total_amount > 200:
+        flat_10_discount()
+    if a > 10 or b > 10 or c > 10:
+        bulk_5_discount()
+    if total_quantity > 20:
+        bulk_10_discount()
+    if total_quantity > 30 and (a > 15 or b > 15 or c > 15):
+        tiered_50_discount()
+    total_wrap_amount = cal_wrap_amount()
+    shipping_fee = cal_shipping_fee()
+    max_discount = find_max_discount()
+    discount_name = find_discount_name(max_discount) if max_discount > 0 else "nill"
+    total_amount = cart_total_amount - max_discount + total_wrap_amount + shipping_fee
+    print()
+    print()
+    print("PRODUCT NAME         QUANTITY        TOTAL AMOUNT")
+    if a:
+        print("PRODUCT A            " + str(a) + "                  " + str(product['Product A'] * a))
+    if b:
+        print("PRODUCT B            " + str(b) + "                  " + str(product['Product B'] * b))
+    if c:
+        print("PRODUCT C            " + str(c) + "                  " + str(product['Product C'] * c))
+    print()
+    print(f"SUBTOTAL :{cart_total_amount}")
+    print(f"DISCOUNT NAME :{discount_name}")
+    print(f"DISCOUNT AMOUNT :{max_discount}")
+    print(f"SHIPPING FEE :{shipping_fee}")
+    print(f"GIFT WRAP FEE :{total_wrap_amount}")
+    print()
+    print(f"TOTAL :{total_amount}")
